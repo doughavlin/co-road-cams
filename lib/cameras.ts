5a77b45d2eefd1e7b5b5dfa8264024b8b30137fb
Doug Havlin <doughavlin@gmail.com>
2025-12-05 16:00:37 -0700

    adding cameras and displaying them

---
+++ lib/cameras.ts
@@ -0,0 +1,19 @@
+export interface Camera {
+    name: string;
+    url: string;
+}
+
+export const cameras: Camera[] = [
+    { name: 'Gunnison - East', url: 'https://cocam.carsprogram.org/Cellular/050E15805CAM1GP1-E.jpg' },
+    { name: 'Gunnison - West', url: 'https://cocam.carsprogram.org/Cellular/050E15805CAM1GP1-W.jpg' },
+    { name: 'Monarch - East', url: 'https://cocam.carsprogram.org/Cellular/050W19935CAM1RHS-E.jpg' },
+    { name: 'Monarch - West', url: 'https://cocam.carsprogram.org/Cellular/050W19935CAM1RHS-W.jpg' },
+    { name: 'Poncha - North', url: 'https://cocam.carsprogram.org/Cellular/285S12625CAM1GP1-N.jpg' },
+    { name: 'Poncha - South', url: 'https://cocam.carsprogram.org/Cellular/285S12625CAM1GP1-S.jpg' },
+    { name: 'Poncha - Road', url: 'https://cocam.carsprogram.org/Cellular/285S12625CAM1GP1-ROAD.jpg' },
+    { name: 'Beuna Vista Cam - East', url: 'https://cocam.carsprogram.org/Cellular/024E21290CAM1SWC-E.jpg' },
+    { name: 'Beuna Vista Cam - North', url: 'https://cocam.carsprogram.org/Cellular/024E21290CAM1SWC-N.jpg' },
+    { name: 'Beuna Vista Cam - South', url: 'https://cocam.carsprogram.org/Cellular/024E21290CAM1SWC-S.jpg' },
+    { name: 'Fairplay - North', url: 'https://cocam.carsprogram.org/Cellular/285N18300CAM1SWC-N.jpg' },
+    { name: 'Fairplay - South', url: 'https://cocam.carsprogram.org/Cellular/285N18300CAM1SWC-S.jpg' },
+];
